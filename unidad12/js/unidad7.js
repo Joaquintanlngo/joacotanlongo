3b29a7007c28431fa6bd7698ba9eb77a1f67347c
Joaquintanlngo <118077542+Joaquintanlngo@users.noreply.github.com>
2022-11-12 17:04:13 -0300

    Add files via upload
    Signed-off-by: Joaquintanlngo <118077542+Joaquintanlngo@users.noreply.github.com>

---
+++ unidad12/js/unidad7.js
@@ -0,0 +1,10 @@
+alert ("Sean bienvenidos a Mas que un antojo")
+var nombreUsuario = prompt ("¿Quiere realizar un pedido?")
+var contacto = prompt ("¿Cuales son tus datos de contacto?")
+var pedido = prompt ("¿Que tipo de torta le gustaria pedir? Preparamos: Rogel, Tofi, Brownie, Coco, Frutal, Pastafrola.")
+var tortas = prompt ("¿Cuantas tortas?")
+var envio = prompt ("¿Necesita que le enviemos el pedido?") 
+
+
+
+
